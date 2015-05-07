1b508b59b0bd1c833628a735c715ca9740620d4c
hrach <hrach.cz@gmail.com>
2015-05-07 19:49:15 +0200

    tests: updated for new api

--- tests/Nextras/YoutubeApi/Reader.phpt
+++ tests/Nextras/YoutubeApi/Reader.phpt
@@ -8,26 +8,31 @@
  * @subpackage UnitTests
  */
 
+use Tester\Assert;
+
 require_once __DIR__ . '/../bootstrap.php';
 
 
+$config = parse_ini_file(__DIR__ . '/../../php.ini');
+$apiKey = $config['googleApiKey'];
 
-$reader = new Nextras\YoutubeApi\Reader;
+$reader = new Nextras\YoutubeApi\Reader($apiKey);
 $video  = $reader->getVideo('wsaPIG6kvlo');
 
 Assert::true($video instanceof Nextras\YoutubeApi\Video);
 Assert::same('Nette Framework and Flash Messages', $video->title);
 Assert::same('http://www.youtube.com/watch?v=wsaPIG6kvlo', $video->url);
-Assert::null($video->description);
-Assert::same(334, $video->duration);
+Assert::same('', $video->description);
+Assert::same(335, $video->duration);
 
-Assert::same(4, count($video->thumbs));
-Assert::true(!empty($video->thumbs[0]->url));
-Assert::true(!empty($video->thumbs[1]->width));
-Assert::true(!empty($video->thumbs[2]->height));
-Assert::true(!empty($video->thumbs[3]->time));
+Assert::same(3, count($video->thumbs));
 
-
+// check only basic
+foreach (['default', 'medium', 'high'] as $type) {
+	Assert::true(!empty($video->thumbs[$type]->url));
+	Assert::true(!empty($video->thumbs[$type]->width));
+	Assert::true(!empty($video->thumbs[$type]->height));
+}
 
 $video2 = $reader->getVideoByUrl($video->url);
 Assert::equal($video, $video2);
--- tests/Nextras/bootstrap.php
+++ tests/Nextras/bootstrap.php
@@ -1,53 +1,9 @@
 <?php
 
-/**
- * Test initialization and helpers.
- *
- * @author     David Grudl
- * @package    Nette\Test
- */
+use Tester\Environment;
 
 
 require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../vendor/nette/tester/Tester/bootstrap.php';
-
-
-// configure environment
 date_default_timezone_set('Europe/Prague');
 
-
-// temporary directory garbage collection
-if (mt_rand() / mt_getrandmax() < 0.01) {
-	foreach (glob(__DIR__ . '/../tmp/*', GLOB_ONLYDIR) as $dir) {
-		if (time() - @filemtime($dir) > 300 && @rename($dir, $dir . '-delete')) {
-			TestHelpers::purge($dir . '-delete');
-			rmdir($dir . '-delete');
-		}
-	}
-}
-
-// create temporary directory
-define('TEMP_DIR', __DIR__ . '/../tmp/' . getmypid());
-TestHelpers::purge(TEMP_DIR);
-
-
-$loader = new Nette\Loaders\RobotLoader;
-$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(TEMP_DIR));
-$loader->addDirectory(__DIR__ . '/../../Nextras');
-$loader->register();
-
-
-$_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
-$_SERVER['REQUEST_TIME'] = 1234567890;
-$_ENV = $_GET = $_POST = array();
-
-
-if (extension_loaded('xdebug')) {
-	xdebug_disable();
-	TestHelpers::startCodeCoverage(__DIR__ . '/coverage.dat');
-}
-
-
-function id($val) {
-	return $val;
-}
+Environment::setup();
